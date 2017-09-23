0367e9e1cabbf5fecc3dbf17411b2dad9c725e1e
ibigbug <akabyw@gmail.com>
2017-09-24 06:45:19 +0800

    add dockerfile

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"flag"
-	"log"
 
+	raven "github.com/getsentry/raven-go"
 	"github.com/ibigbug/ss-account/config"
 	"github.com/ibigbug/ss-account/server"
 )
+
+func init() {
+	raven.SetDSN("https://3913f327a7ef45f5af635b8bcbc2e0a4:981cefa1e6a940eeba684abb446609d1@sentry.io/163096")
+
+}
 
 var bind = flag.String("bind", "0.0.0.0:9000", "management server listening address")
 var portRange = flag.String("port-range", "30000-40000", "accounting port range, e.g: 30000-40000")
@@ -19,5 +24,5 @@ func main() {
 	if *bind == "" {
 		*bind = "127.0.0.1:9000"
 	}
-	log.Fatalln(server.Start(*bind))
+	raven.CapturePanic(server.Start(*bind), nil)
 }
