19db4473b3dddd321d815a8899e91963bfa0ce18
ibigbug <akabyw@gmail.com>
2017-10-29 17:13:06 +0800

    add artifacts

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,23 @@
-import requests
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-proxy = dict(
-    http='socks5:127.0.0.1:1080',
-    https='socks5:127.0.0.1:1080',
-)
 
-req = requests.get('http://ip.cn', proxies=proxy)
-print(req.text)
+class LoadproviderHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+
+        # 512 Mb
+        for i in range(1024 * 512):
+            self.wfile.write('x' * 1024)
+
+
+def run(server_class=HTTPServer, handler_class=LoadproviderHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd at %s:%s' % server_address)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
